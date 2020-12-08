d58ab4bda0e24d4cb73a13f6f6fa9a84803fe869
kiyoungsong <thdrldud369@gmail.com>
2020-12-08 21:40:59 +0900

    초기 커밋.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using IdeaAppCore.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace IdeaAppCore.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Idea> Ideas { get; set; }
+    }
+}
---
+++ Models/Idea.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace IdeaAppCore.Models
+{
+    public class Idea
+    {
+        public int ID { get; set; }
+        
+        [Required]
+        public string Note { get; set; }
+    }
+}
